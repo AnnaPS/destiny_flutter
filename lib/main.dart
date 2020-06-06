675b5a64a27ce79cb62ae92b48484527c0a10faa
AnnaPS <anna-91_@hotmail.com>
2020-06-06 16:49:21 +0200

    added logic to misterry game, added sounds, animations and button decorations

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,62 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+import 'story_brain.dart';
+import 'package:flutter/animation.dart';
 
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: StoryPage(),
     );
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
-
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
 }
 
-class _StoryPageState extends State<StoryPage> {
+class _StoryPageState extends State<StoryPage> with TickerProviderStateMixin {
+  StoryBrain storyBrain = StoryBrain();
+
+  AnimationController controller;
+  Animation animation;
+  final AudioCache player = AudioCache();
+  _asyncAnimation() async {
+    controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    //this will start the animation
+    await controller.forward();
+  }
+
+  _playSound() {
+    final AudioCache player = AudioCache();
+    player.loop('mistery.wav');
+  }
+
+  @override
+  initState() {
+    super.initState();
+    _asyncAnimation();
+    _playSound();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        //TODO: Step 1 - Add background.png to this Container as a background image.
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/forest.png'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.4), BlendMode.darken)),
+        ),
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
@@ -34,28 +66,51 @@ class _StoryPageState extends State<StoryPage> {
               Expanded(
                 flex: 12,
                 child: Center(
-                  child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
-                    style: TextStyle(
-                      fontSize: 25.0,
+                  child: FadeTransition(
+                    opacity: animation,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        storyBrain.getStory(),
+                        style: TextStyle(fontSize: 25.0, letterSpacing: 2.0),
+                      ),
                     ),
                   ),
                 ),
               ),
               Expanded(
                 flex: 2,
-                child: FlatButton(
+                child: RaisedButton(
+                  color: Colors.transparent,
                   onPressed: () {
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(1);
+                      _asyncAnimation();
+                    });
                   },
-                  color: Colors.red,
-                  child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
-                    style: TextStyle(
-                      fontSize: 20.0,
+                  child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xFFE57373),
+                            Color(0xFFD32F2F),
+                            Color(0xFFD50000),
+                          ],
+                        ),
+                        boxShadow: [
+                          BoxShadow(color: Colors.white, spreadRadius: 2)
+                        ],
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20.0),
+                            bottomRight: Radius.circular(20.0))),
+                    child: Text(
+                      storyBrain.getChoice1(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
@@ -65,19 +120,37 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                  },
-                  color: Colors.blue,
-                  child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
-                    style: TextStyle(
-                      fontSize: 20.0,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: RaisedButton(
+                    color: Colors.transparent,
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                        _asyncAnimation();
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(20.0),
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFFDD835),
+                              Color(0xFFF9A825),
+                              Color(0xFFFF6F00),
+                            ],
+                          ),
+                          boxShadow: [
+                            BoxShadow(color: Colors.white, spreadRadius: 2)
+                          ],
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20.0),
+                              bottomRight: Radius.circular(20.0))),
+                      child: Text(
+                        storyBrain.getChoice2(),
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
@@ -88,8 +161,10 @@ class _StoryPageState extends State<StoryPage> {
       ),
     );
   }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
